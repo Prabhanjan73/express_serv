e2afa4934bfc129cdebfad58c64ca725122c3c00
Prabhanjan73 <40603897+Prabhanjan73@users.noreply.github.com>
2025-04-20 17:41:10 +0530

    Add files via upload

--- new.js
+++ new.js
@@ -1,15 +1,21 @@
 // 1. Import express
 const express = require('express');
 
+require('dd-trace').init();
+
 // 2. Create an Express app
 const app = express();
 
 // 3. Choose a port number
-const PORT = 4000;
+const PORT = 8080;
 
 // 4. Define a route for GET requests to the homepage
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, world! Starting  Datadog');
 });
 
 // 5. Start the server and listen on the chosen port
